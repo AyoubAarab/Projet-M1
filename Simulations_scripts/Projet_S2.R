62e0febaf568608563d59b3502662844654750e1
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-03-11 23:34:15 +0100

    Update Projet_S2.R

--- Simulations_scripts/Projet_S2.R
+++ Simulations_scripts/Projet_S2.R
@@ -4,22 +4,22 @@
 #mu=10
 #gamma=1
 #sigma=0.5
-n=50
+
 mu=0.2
 gamma=0.3
 sigma=0.5
-Z <- runif(n, 15, 80)
-U <- runif(n, 0, 1)
+Z <- as.numeric(runif(50, 15, 80) < 50)
+U <- runif(50, 0, 1)
 W <- log(-log(1-U))
-X <- (mu+ gamma*Z+ sigma*W)
+X <- exp(mu+ gamma*Z+ sigma*W)
 
-T=rweibull(n, 1/sigma, (mu+gamma*Z))
+T=rweibull(50, 1/sigma, exp(mu+gamma*Z))
 #View(T)
-View(X)
+#View(X)
 
 
 mu=0.1
-C=rexp(n,mu)
+C=rexp(50,mu)
 
 time=pmin(X,C)
 #indicatrice de l'évènement d'intérêt
@@ -28,6 +28,14 @@ time=pmin(X,C)
 #ou bien
 status=as.numeric(X<C)
 #View(Time)
-D<-data.frame(id=1:n, time = time, status=status, Z=Z)
+D<-data.frame(id=1:50, time = time, status=status, Z=Z)
 D
 View(D)
+
+library(survival)
+library(survminer)
+
+f<- survfit(Surv(time, status) ~ Z, type = "kaplan-meier", conf.type = "plain", D)
+ggsurvplot(f, data = D)
+ggsurvplot(f, fun = "cloglog", data = D, xlim=c(-50,50))
+
