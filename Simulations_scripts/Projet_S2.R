0d5314f4bc5f00f1f17f32a9eb2496515edfeecb
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-03-11 23:03:23 +0100

    Update Projet_S2.R

--- Simulations_scripts/Projet_S2.R
+++ Simulations_scripts/Projet_S2.R
@@ -1,33 +1,33 @@
 #log(X)=mu+gammaZ+sigmaW
-#Où Z ~ unif[15,80]
-#W=log(-log(1-U)) où U ~ unif[0,1]
+#OÃ¹ Z ~ unif[15,80]
+#W=log(-log(1-U)) oÃ¹ U ~ unif[0,1]
 #mu=10
 #gamma=1
 #sigma=0.5
-
+n=50
 mu=0.2
 gamma=0.3
 sigma=0.5
-Z <- runif(50, 15, 80)
-U <- runif(50, 0, 1)
+Z <- runif(n, 15, 80)
+U <- runif(n, 0, 1)
 W <- log(-log(1-U))
 X <- (mu+ gamma*Z+ sigma*W)
 
-T=rweibull(50, 1/sigma, exp(mu+gamma*Z))
+T=rweibull(n, 1/sigma, (mu+gamma*Z))
 #View(T)
 View(X)
 
 
 mu=0.1
-C=rexp(50,mu)
+C=rexp(n,mu)
 
 time=pmin(X,C)
-#indicatrice de l'évènement d'intérêt
+#indicatrice de l'Ã©vÃ¨nement d'intÃ©rÃªt
 #delta vaut 1 si X<c et O sinon
 #delta=as.numeric(TT==X)
 #ou bien
 status=as.numeric(X<C)
 #View(Time)
-D<-data.frame(id=1:50, time = time, status=status, Z=Z)
+D<-data.frame(id=1:n, time = time, status=status, Z=Z)
 D
 View(D)
